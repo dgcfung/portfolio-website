4dd88a389a5481b983874f0ed6a75db810a53090
B Legit <d.gc.fung@gmail.com>
2020-03-23 21:45:32 -0400

    update links

--- src/components/Project.js
+++ src/components/Project.js
@@ -14,6 +14,8 @@ function Project(){
         <p>Currency Exchange Rate app was designed to allow users a quick check of latest exchange rates. Latest news will also appear in the feed to give users a convenient way to keep up to date.</p>
         <h4>Languages:</h4>
         <p>HTML, CSS, Javascript</p>
+        <a href="https://pages.git.generalassemb.ly/dgcfung/currency-exchange-rate/">Link To Project</a>
+        
         </div>
         <div className= "project-div">
         <img className="projects" style={{height: "50vh", width: "50vw" }}src= {DictionaryProject}/> 
@@ -21,6 +23,7 @@ function Project(){
         <p>Dictionary app utilized the Merriam-Webster's dictionary to allow users a quick way to look up latest words.</p>
         <h4>Languages:</h4>
         <p>React, CSS, Merriam-Webster's API</p>
+        <a href="http://dictionary-app-donald.surge.sh/">Link To Project</a>
         </div>
         <div className="project-div">
         <img className="projects" style={{height: "50vh", width: "50vw" }}src= {Megabus}/> 
@@ -29,12 +32,14 @@ function Project(){
         <p>Following Agile & Scrum methodology, a collaborative project to re-design Megabus's mobile app.</p>
         <h4>Languages:</h4>
         <p>React, CSS, Express.js, Sequelize, PostgreSQL</p>
+        <a href="http://megabus-mobile.herokuapp.com/">Link To Project</a>
         <div className= "project-div">
-        <img className="projects" style={{height: "50vh", width: "50vw" }}src= {BloggerProject}/> 
+       <img className="projects" style={{height: "50vh", width: "50vw" }}src= {BloggerProject}/> 
         <h3>Blogger 2020</h3>
         <p>Blogger 2020 is a new app allowing users to share their feelings with their friends, family, and co-workers.</p>
         <h4>Languages:</h4>
-        <p>React, Ruby on Rails, PostgreSQL, bcrypt, JWT</p>
+        <p>React, Ruby on Rails, PostgreSQL, bcrypt, JWT</p> 
+        <a href="https://git.generalassemb.ly/dgcfung/blogger-2020-backup">Link To Project</a>
         </div>
 
         </div>
