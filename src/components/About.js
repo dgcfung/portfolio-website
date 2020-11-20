8c5586eece32a0e2842bed38d237bcb9d73433dd
B Legit <d.gc.fung@gmail.com>
2020-11-20 18:40:43 -0500

    resume update

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ function About() {
             {/* <a className= "resume" href= "https://www.docdroid.net/Rc9QKLN/donald-fung-pdf">Click Here for My Resume</a> */}
             <p className="brand-statement">I am a highly motivated software developer with a passion to connect users with technology with a background in marketing, sales, and academia. My past experience as a researcher and regional account manager have strengthened my skills working in a leadership role and working in a collaborative environment. These experiences have allowed me to combine my passion for creative products with my love for coding, programming, and learning.</p>
             <br></br>
-            <a className= "resume" href= "https://www.docdroid.net/ZeLUMV1/donald-fung-pdff">Please Click Here for My Full Resume</a>
+            <a className= "resume" href= "https://docs.google.com/document/d/1HG2_km4R2EUw6wPIj3ZQhBNez5Va3GeimwmdEbkTewM/edit?usp=sharing">Please Click Here for My Full Resume</a>
             </div>
         </div>
     )
