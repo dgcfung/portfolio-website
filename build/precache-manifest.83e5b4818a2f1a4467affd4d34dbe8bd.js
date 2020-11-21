5cfdc884a58c3dfe4f2cad20a897dfd070250961
B Legit <d.gc.fung@gmail.com>
2020-11-20 19:23:06 -0500

    resume update

---
+++ build/precache-manifest.83e5b4818a2f1a4467affd4d34dbe8bd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba9da2bee1c69fa56d2272a00b577e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4884b6149d66bc996410",
+    "url": "/static/css/main.b1ef8d08.chunk.css"
+  },
+  {
+    "revision": "6a28000bfbf9d4574fb0",
+    "url": "/static/js/2.caa40462.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.caa40462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4884b6149d66bc996410",
+    "url": "/static/js/main.1473a0bf.chunk.js"
+  },
+  {
+    "revision": "285a02fd36a32da3dcfe",
+    "url": "/static/js/runtime-main.70b058cc.js"
+  },
+  {
+    "revision": "27b4d9dfc8404f473f29834c9758e8a3",
+    "url": "/static/media/BloggerProject.27b4d9df.jpg"
+  },
+  {
+    "revision": "0f02e132b7df9d9ade71d87b5c7c8141",
+    "url": "/static/media/CSSLogo.0f02e132.png"
+  },
+  {
+    "revision": "33cf9cba082cd3947736a41934197049",
+    "url": "/static/media/CurrencyExchange.33cf9cba.jpg"
+  },
+  {
+    "revision": "70bcc99d325f1883732cdf5f5cd72de8",
+    "url": "/static/media/DictionaryProject2.70bcc99d.png"
+  },
+  {
+    "revision": "a7035066f9585a33850ccdcb3f0ceb0f",
+    "url": "/static/media/DonaldProfilePic2.a7035066.jpg"
+  },
+  {
+    "revision": "61b2ad5f71e3b5aff667399326f629c5",
+    "url": "/static/media/EmailLogo.61b2ad5f.png"
+  },
+  {
+    "revision": "98ff4822d03ab0f5746653f56441d38e",
+    "url": "/static/media/ExpressLogo.98ff4822.png"
+  },
+  {
+    "revision": "d19bbdb4c119b7d984414179907a58d1",
+    "url": "/static/media/GitHubLogo.d19bbdb4.png"
+  },
+  {
+    "revision": "456f9ad76661687332199f0aaaa189da",
+    "url": "/static/media/HTMLLogo2.456f9ad7.png"
+  },
+  {
+    "revision": "fc66ffc4af3072ea27eb71b5180addcd",
+    "url": "/static/media/JSLogo.fc66ffc4.jpg"
+  },
+  {
+    "revision": "849d9fafedaed2e22c6d6212b2cc9a09",
+    "url": "/static/media/MegaBusMobile.849d9faf.jpg"
+  },
+  {
+    "revision": "60065f054dfdd08e0491b3dcd8c89177",
+    "url": "/static/media/NodeLogo.60065f05.png"
+  },
+  {
+    "revision": "8e4e44c5eb6bc0cf86fd70a212051329",
+    "url": "/static/media/PostgreSQLLogo.8e4e44c5.png"
+  },
+  {
+    "revision": "5e72d705930d544f48a07407a3ef8f6f",
+    "url": "/static/media/RailsLogo.5e72d705.png"
+  },
+  {
+    "revision": "6a43c490573bab7a1d2d557daeefb863",
+    "url": "/static/media/RubyLogo.6a43c490.png"
+  },
+  {
+    "revision": "2019a33262db9c492be65728bd4e6053",
+    "url": "/static/media/SequelizeLogo.2019a332.png"
+  }
+]);
